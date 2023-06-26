ca61007c97596c5daebd06c4f547de640c73dd66
vuluu <vuluu040320@gmail.com>
2023-06-26 17:04:39 +0700

    update

--- src/App.vue
+++ src/App.vue
@@ -13,6 +13,11 @@
       ColorPickerCustom,
       ColorGradient,
     },
+    methods: {
+      handleChangeGradient(gradient) {
+        this.background = gradient
+      },
+    },
     watch: {
       color(value) {
         const selection = window.getSelection()
@@ -40,12 +45,12 @@
     <p style="margin-bottom: 12px, min-width: 600px">
       Hello everyone, You can selection text and change color with color picker
     </p>
-
-    <div class="background-test"></div>
-
     <ColorPickerCustom v-model:value="color" v-model:selection="selection" />
+
+    <div class="background-test" :style="{ background }"></div>
+
     <div class="wrapper">
-      <ColorGradient />
+      <ColorGradient @change="handleChangeGradient" />
     </div>
   </main>
 </template>
@@ -64,8 +69,13 @@
     width: fit-content;
     box-shadow: rgba(0, 0, 0, 0.24) 0px 3px 8px;
   }
-  
-  .background-test{
-    
+
+  .background-test {
+    width: 400px;
+    height: 200px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    margin: 12px;
   }
 </style>
